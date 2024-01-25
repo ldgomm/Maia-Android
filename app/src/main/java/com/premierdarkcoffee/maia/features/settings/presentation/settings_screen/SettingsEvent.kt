023afc76a18e7a407193fb00580079567fe0c891
ldgomm <ruiz211@protonmail.com>
2024-01-25 10:14:12 -0500

    Working on Settings

--- app/src/main/java/com/premierdarkcoffee/maia/features/settings/presentation/settings_screen/SettingsEvent.kt
+++ app/src/main/java/com/premierdarkcoffee/maia/features/settings/presentation/settings_screen/SettingsEvent.kt
@@ -1,4 +1,4 @@
-package com.premierdarkcoffee.hermes.features.settings.presentation.settings_screen
+package com.premierdarkcoffee.maia.features.settings.presentation.settings_screen
 
 sealed class SettingsEvent {
     data class DarkModeToggled(val isDarkTheme: Boolean) : SettingsEvent()
