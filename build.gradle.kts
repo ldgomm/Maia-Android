67aceb0c2946fda2e0b477ec7de1ae274fba8544
ldgomm <ruiz211@protonmail.com>
2024-02-21 17:13:19 -0500

    Dependencies update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "7.3.1" apply false
 
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 
     id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
